13ee96409e89c96b3903da4bf495e9225670c514
sakethanne <36626206+Sakethanne@users.noreply.github.com>
2024-04-26 23:00:41 -0700

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,8 +1,10 @@
 import { Config } from "./src/config";
 
 export const defaultConfig: Config = {
-  url: "https://www.builder.io/c/docs/developers",
-  match: "https://www.builder.io/c/docs/**",
+  // url: "https://www.builder.io/c/docs/developers",
+  // match: "https://www.builder.io/c/docs/**",
+  url: "https://www.tableau.com",
+  match: "https://www.tableau.com/**",
   maxPagesToCrawl: 50,
   outputFileName: "output.json",
   maxTokens: 2000000,
